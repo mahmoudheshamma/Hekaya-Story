d950cdab1e25aef09af637dfe42b4c63f3237d0b
mahmoudheshamma <mh20232293@sva.edu.eg>
2026-01-12 13:09:57 +0200

    Update

---
+++ js/firebaseConfig.js
@@ -0,0 +1,21 @@
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAwYs3xZW84Yy0Z2_DXk8FLUBJtShDQUoA",
+    authDomain: "story-1cfce.firebaseapp.com",
+    databaseURL: "https://story-1cfce-default-rtdb.firebaseio.com",
+    projectId: "story-1cfce",
+    storageBucket: "story-1cfce.firebasestorage.app",
+    messagingSenderId: "982243872156",
+    appId: "1:982243872156:web:203a1901a90ee8af998c59",
+    measurementId: "G-M19HVL0RZ2"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export { app, analytics, database };
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// main.js
+import { app, analytics, database } from './firebaseConfig.js';
+
+// مثال استخدام Firebase
+console.log("Firebase Initialized:", app);
